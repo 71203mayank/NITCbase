49804235c49af1ec20795d3c01a297227a551515
71203mayank <mayank71203@gmail.com>
2024-08-02 15:11:17 +0530

    stage two

--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -6,38 +6,40 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-  /* Initialize the Run Copy of Disk */
-  // Disk disk_run;
-  // StaticBuffer buffer;
-  // OpenRelTable cache;
-
-  // return FrontendInterface::handleFrontend(argc, argv);
-
   Disk disk_run;
 
-  unsigned char buffer[BLOCK_SIZE];
-  Disk::readBlock(buffer, 7000);
-  char message[] = "hello";
+  //Create objects for the relation catalog and attribute catalog
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
 
-  //memcpy(to*, from*, size)
-  memcpy(buffer + 20, message, 6);
-  Disk::writeBlock(buffer, 7000);
+  HeadInfo relCatHeader;
+  HeadInfo attrCatHeader;
 
-  unsigned char buffer2[BLOCK_SIZE];
-  char message2[6];
-  Disk::readBlock(buffer2, 7000);
-  memcpy(message2, buffer2 + 20, 6);
-  cout<<message2<<endl;
+  // load the headers of both the blocks into relCatHeader and attrCatHeader
+  // will be implementing these function in future stages
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
+  for(int i = 0; i < relCatHeader.numEntries; i++){
 
-  //Exercise: Reading form block allocation map
-  unsigned char buffer3[BLOCK_SIZE];
-  char message3[10];
-  Disk::readBlock(buffer3,0);
-  memcpy(message3,buffer3,10);
-  // cout<<message3<<endl;
-  for(auto x : message3){
-    cout<<(int)x<<" ";
+    Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
+    relCatBuffer.getRecord(relCatRecord, i);
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+    for(int j = 0; j < attrCatHeader.numEntries; j++){
+
+      //Declaring attrCatRecord and loading the attribute catalog entry into it
+      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+      attrCatBuffer.getRecord(attrCatRecord,j);
+
+      // if attribute catalog entry corresponds to the current relation
+      if(strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal , relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0){
+        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER? "NUM" : "STR";
+        printf(" %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+      }
+    }
+    printf("\n");
   }
+  
   return 0;
   
 
