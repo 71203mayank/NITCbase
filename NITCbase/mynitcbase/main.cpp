4c72fb824f7b4b2c8e877d19b46f4a26f84a4257
71203mayank <mayank71203@gmail.com>
2024-08-03 01:45:19 +0530

    solved stage2 both problems

--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -19,6 +19,13 @@ int main(int argc, char *argv[]) {
   // will be implementing these function in future stages
   relCatBuffer.getHeader(&relCatHeader);
   attrCatBuffer.getHeader(&attrCatHeader);
+
+  // Modification question 1: two read across multiple records
+  // We will have to create new recordBuffer and headInfo
+  HeadInfo attrCatHeader2;
+  RecBuffer attrCatBuffer2(attrCatHeader.rblock);
+  attrCatBuffer2.getHeader(&attrCatHeader2);
+
   for(int i = 0; i < relCatHeader.numEntries; i++){
 
     Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
@@ -31,6 +38,34 @@ int main(int argc, char *argv[]) {
       Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
       attrCatBuffer.getRecord(attrCatRecord,j);
 
+
+      // Modification Question 2, Change attribute name 'Class' of Student to 'Batch' 
+      if(strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,"Students") == 0){
+        if(strcmp(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal,"Class") == 0){
+          unsigned char temp_buffer[BLOCK_SIZE];
+          Disk :: readBlock(temp_buffer,ATTRCAT_BLOCK);
+          int recordSize = ATTR_SIZE*ATTRCAT_NO_ATTRS;
+          unsigned char* newAttrName = temp_buffer + HEADER_SIZE + attrCatHeader.numSlots + recordSize*j + ATTRCAT_ATTR_NAME_INDEX*ATTR_SIZE;
+          // basically : 0 + 32 + 20 + 96*j + 1*16
+          memcpy(newAttrName,"Batch",ATTR_SIZE);
+          Disk :: writeBlock(temp_buffer, ATTRCAT_BLOCK);
+        }
+      }
+
+      // if attribute catalog entry corresponds to the current relation
+      if(strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal , relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0){
+        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER? "NUM" : "STR";
+        printf(" %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+      }
+    }
+
+    // 2nd for loop to print 2nd block of the attribute catalog
+    for(int j = 0; j < attrCatHeader2.numEntries; j++){
+
+      //Declaring attrCatRecord and loading the attribute catalog entry into it
+      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+      attrCatBuffer2.getRecord(attrCatRecord,j);
+
       // if attribute catalog entry corresponds to the current relation
       if(strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal , relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0){
         const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER? "NUM" : "STR";
@@ -38,6 +73,7 @@ int main(int argc, char *argv[]) {
       }
     }
     printf("\n");
+    
   }
   
   return 0;
